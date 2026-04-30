212960c289d4b373de4913b6df45e5c2bfad9a1f
Manan patel <immnan333@gmail.com>
2026-04-30 17:01:42 +0530

    update

--- container/build.sh
+++ container/build.sh
@@ -3,9 +3,9 @@
 #!/bin/sh
 
 sudo docker buildx build --platform linux/amd64 --load -f container/Dockerfile . -t "immnan/hawk-apiserver:0.1.0"
-sudo docker tag "immnan/hawk-apiserver:0.1.0" "immnan/hawk-apiserver:latest"
+sudo docker tag "immnan/hawk-apiserver:0.1.1" "immnan/hawk-apiserver:latest"
 
 # Pushing this to my repo for now. 
-sudo docker push "immnan/hawk-apiserver:0.1.0"
+sudo docker push "immnan/hawk-apiserver:0.1.1"
 sudo docker images
 
